35030fd5c72ab49c558e1f1e6c6c0a2c0d8025cb
Azimjon Walker <998916379677artur@gmail.com>
2018-05-04 21:19:15 +0500

    create project

---
+++ app/src/main/java/edu/azimjon/gallery_project_for_yandex/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.azimjon.gallery_project_for_yandex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+}
